35b5090e41101e3b7935c2d1d3ec3b2382ec5b3b
cparros <celparros@gmail.com>
2021-03-01 14:27:39 -0500

    added role table

--- schema.sql
+++ schema.sql
@@ -6,8 +6,17 @@ USE employee_tracker_db;
 
 CREATE TABLE employee (
 id INT NOT NULL auto_increment,
-first_name varchar(30),
-last_name varchar(30),
-role_id INT 
-manager_id INT
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT, 
+manager_id INT,
+PRIMARY KEY (id)
+)
+
+CREATE TABLE role (
+  id INT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
 )
